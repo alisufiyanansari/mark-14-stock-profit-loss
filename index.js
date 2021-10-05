f2c529e29d6e2bb955a0fdcb7b3278503e503165
Ali Suffian <1414sufiyan@gmail.com>
2021-10-05 23:02:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const form = document.getElementById('mainform')
-const initialPrice = document.querySelector('#intialprice')
-const stocksQuantity = document.querySelector('#quatityofstocks')
-const currentPrice = document.querySelector('#currentprice')
-const message = document.querySelector('#message-container')
+const form = document.getElementById("mainform")
+const initialPrice = document.querySelector("#intialprice")
+const stocksQuantity = document.querySelector("#quatityofstocks")
+const currentPrice = document.querySelector("#currentprice")
+const message = document.querySelector("#message-container")
 
 function onSubmit(event) {
   event.preventDefault()
@@ -11,7 +11,7 @@ function onSubmit(event) {
   const quantityOfStocks = parseFloat(stocksQuantity.value)
   try {
     if (isNaN(initialValue) || isNaN(currentValue) || isNaN(quantityOfStocks)) {
-      throw new Error('Please enter valid values')
+      throw new Error("Please enter valid values")
     }
     if (initialValue > currentValue) {
       var loss = (initialValue - currentValue) * quantityOfStocks
@@ -38,4 +38,4 @@ function showMessage(msg) {
   message.innerText = msg
 }
 
-form.addEventListener('submit', onSubmit)
+form.addEventListener("submit", onSubmit)
